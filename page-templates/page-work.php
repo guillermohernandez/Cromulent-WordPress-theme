0251a6c274e297f3d41a4c51dd8b82bef606b1e7
Guillermo Hernandez <guillermohernandez@gmail.com>
2017-03-15 21:50:35 -0500

    work page

--- page-templates/page-work.php
+++ page-templates/page-work.php
@@ -42,7 +42,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 								<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
 								<div class="card text-center">
 									<a href="<?php the_permalink(); ?>">
-									<img class="card-img-top img-fluid" src="<?php the_field('project_img_1'); ?>" />
+									<img class="card-img-top img-fluid" src="<?php the_field('work_thumb'); ?>" />
 									</a>
 								<div class="card-block card-inverse card-primary">
 								<?php the_title( '<h4 class="card-title">', '</h4>' ); ?>
