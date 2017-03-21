2bdeae37e80c290cb41fb475ff6b9c708f38079c
Guillermo Hernandez <guillermohernandez@gmail.com>
2017-03-20 19:33:28 -0500

    enqueue fix attempt

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -44,12 +44,3 @@ function cromulent_dequeue_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'cromulent_dequeue_scripts', 99 );
-
-function google_fonts() {
-	$query_args = array(
-		'family' => 'Open+Sans:300,400,600|Raleway:800'
-	);
-	wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
-            }
-            
-add_action('wp_enqueue_scripts', 'google_fonts');
