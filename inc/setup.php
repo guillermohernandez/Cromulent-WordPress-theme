6f778764e697cd833cfdf20dd9648519513404cc
Guillermo Hernandez <guillermohernandez@gmail.com>
2017-04-02 13:06:51 -0500

    dark button style

--- inc/setup.php
+++ inc/setup.php
@@ -118,7 +118,7 @@ if ( ! function_exists( 'all_excerpts_get_more_link' ) ) {
 	 */
 	function all_excerpts_get_more_link( $post_excerpt ) {
 
-		return $post_excerpt . ' [...]<p><a class="btn btn-secondary understrap-read-more-link" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Read Post',
+		return $post_excerpt . ' [...]<p><a class="btn btn-dark" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Read Post',
 		'understrap' ) . '</a></p>';
 	}
 }
