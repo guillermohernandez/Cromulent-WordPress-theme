1414a7bb5494e346693545fad784d19e34421ce4
Guillermo Hernandez <guillermohernandez@gmail.com>
2017-04-05 01:27:56 -0500

    added google fonts to enqueue

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * Understrap enqueue scripts
+ * cromulent enqueue scripts
  *
- * @package understrap
+ * @package cromulent
  */
 
-if ( ! function_exists( 'understrap_scripts' ) ) {
+if ( ! function_exists( 'cromulent_scripts' ) ) {
 	/**
 	 * Load theme's JavaScript sources.
 	 */
-	function understrap_scripts() {
+	function cromulent_scripts() {
 		// Get the theme data.
 		$the_theme = wp_get_theme();
-		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $the_theme->get( 'Version' ) );
+		wp_enqueue_style( 'cromulent-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $the_theme->get( 'Version' ) );
 		wp_enqueue_script( 'jquery' );
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'cromulent-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
 	}
-} // endif function_exists( 'understrap_scripts' ).
+} // endif function_exists( 'cromulent_scripts' ).
 
-add_action( 'wp_enqueue_scripts', 'understrap_scripts' );
+add_action( 'wp_enqueue_scripts', 'cromulent_scripts' );
 
 // Load Contact Form 7 on contact page only
 function cromulent_dequeue_scripts() {
@@ -45,6 +45,15 @@ function cromulent_dequeue_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'cromulent_dequeue_scripts', 99 );
+
+//load google fonts
+
+function cromulent_google_fonts() {
+	wp_register_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600|Raleway:800');
+	wp_enqueue_style( 'google-fonts');
+	}
+add_action('wp_enqueue_scripts', 'cromulent_google_fonts');
+
 
 // Remove WP Embed
 function cromulent_deregister_scripts(){
